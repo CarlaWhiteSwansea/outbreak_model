739673d3dfdbc6cd0014cba7bbd757356f33288a
carlalwhite <c.l.white@swansea.ac.uk>
2022-05-30 14:44:46 +0100

    Added median value to plot

--- scripts/monkeypox_single_scenario.R
+++ scripts/monkeypox_single_scenario.R
@@ -2,7 +2,7 @@ library(outbreak)
 library(dplyr)
 library(ggplot2)
 
-results <- scenario_sim(n.sim = 400,
+results <- scenario_sim(n.sim = 1000,
                     num.initial.cases = 1,
                     prop.asym=0,
                     cap_cases = 4500,
@@ -10,7 +10,7 @@ results <- scenario_sim(n.sim = 400,
                     r0isolated = 1.2,
                     r0community = 0.2,
                     disp.com = 0.16,
-                    disp.iso = 1.5,
+                    disp.iso = 1.6,
                     dist_shape = 2.101547,
                     dist_scale = 11.29064,
                     delay_shape = 1.347738,
@@ -25,7 +25,7 @@ gg <- results %>%
   scale_y_continuous(name="Number of cases")
 
 outbreak_sims <- results %>%
-  filter(day==300 & cumulative > 100) %>%
+  filter(day==9 & cumulative > 10) %>%
   select(sim)
 
 outbreak_results <- results %>%
@@ -33,15 +33,16 @@ outbreak_results <- results %>%
 
 outbreak_mean <- outbreak_results %>%
   group_by(day) %>%
-  summarise(mean=mean(cumulative))
+  summarise(mean=mean(cumulative),median=median(cumulative))
 # outbreak::extinct_prob(res,cap_cases = 4500)
 
 gg <- gg+
-  geom_line(data=outbreak_mean,aes(x=day,y=mean))
+  geom_line(data=outbreak_mean,aes(x=day,y=mean),colour="black")+
+  geom_line(data=outbreak_mean,aes(x=day,y=median),colour="blue")
 
 data <- read.csv("data/monkeypox_uk.csv")
 
 gg <- gg+
-  geom_point(data=data,aes(x=day,y=cumulative))
+  geom_point(data=data,aes(x=day-7,y=cumulative))
 
 gg
